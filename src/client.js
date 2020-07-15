9cbca62fe344f3bb7ed68f906639de2f4a28a773
Markus Felten <Markus.Felten@gmx.de>
2020-07-15 23:06:22 +0200

    test: svelte-websocket-store

--- src/client.js
+++ src/client.js
@@ -1,5 +1,9 @@
 import * as sapper from '@sapper/app';
 
+import websocketStore from 'svelte-websocket-store';
+
+export const myStore = websocketStore("wss://mydomain.com/ws1", {});
+
 sapper.start({
 	target: document.querySelector('#sapper')
 });
